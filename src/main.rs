a75f4a8c5d2a1ee34253cd112f3c5dbc5496babc
Sosthene00 <674694@protonmail.ch>
2022-09-13 23:19:54 +0200

    Add piston_window and some boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use piston_window::{WindowSettings, PistonWindow};
+use piston_window::{clear, rectangle};
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow = WindowSettings::new(
+        "Tetris",
+        (640, 480))
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+    
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g, _| {
+            clear([0.5, 0.5, 0.5, 1.0], g);
+            rectangle([1.0, 0.0, 0.0, 1.0],
+                        [0.0, 0.0, 100.0, 100.0],
+                        c.transform, g);
+        });
+    }
 }
